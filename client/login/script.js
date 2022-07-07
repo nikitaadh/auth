5063bc285a1e51674623689752bce9d00fa402fb
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-07-08 00:05:20 +0545

    Api integration

--- client/login/script.js
+++ client/login/script.js
@@ -1,33 +1,35 @@
-let email = document.getElementById("email");
-let password = document.getElementById("password");
+const API_BASE_URL = 'http://localhost:3001/api';
 
-let emailError = document.getElementById("emailerror");
-let passwordError = document.getElementById("passworderror");
+let email = document.getElementById('email');
+let password = document.getElementById('password');
 
-let successText = document.getElementById("successtext");
+let emailError = document.getElementById('emailerror');
+let passwordError = document.getElementById('passworderror');
 
-let loginForm = document.getElementById("loginform");
-let loginButton = document.getElementById("loginbutton");
+let helperText = document.getElementById('helpertext');
 
-loginButton.addEventListener("click", (e) => {
+let loginForm = document.getElementById('loginform');
+let loginButton = document.getElementById('loginbutton');
+
+loginButton.addEventListener('click', (e) => {
   let errors = {};
 
   let emailPattern =
     /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
   //   let passwordPattern = "^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{8,}$";
 
-  if (email.value === "") {
-    errors.email = "Email is required";
+  if (email.value === '') {
+    errors.email = 'Email is required';
   } else if (!emailPattern.test(email.value)) {
-    errors.email = "Invalid Email";
+    errors.email = 'Invalid Email';
   }
 
-  if (password.value === "") {
-    errors.password = "Password is required";
+  if (password.value === '') {
+    errors.password = 'Password is required';
   } else if (password.value.length < 8) {
-    errors.password = "Password is too short";
+    errors.password = 'Password is too short';
   } else if (password.value.length > 20) {
-    errors.password = "Password is too long";
+    errors.password = 'Password is too long';
     //   } else if (!passwordPattern.test(password.value)) {
     //     errors.password = "Invalid Password";
   }
@@ -36,18 +38,53 @@ loginButton.addEventListener("click", (e) => {
     if (errors.email) {
       emailError.innerText = errors.email;
     } else {
-      emailError.innerText = "";
+      emailError.innerText = '';
     }
     if (errors.password) {
       passwordError.innerText = errors.password;
     } else {
-      passwordError.innerText = "";
+      passwordError.innerText = '';
     }
   }
 
   if (Object.keys(errors).length === 0) {
-    emailError.innerText = "";
-    passwordError.innerText = "";
-    successText.innerText = "Login successful";
+    emailError.innerText = '';
+    passwordError.innerText = '';
+    loginUser();
   }
 });
+
+const loginUser = async () => {
+  try {
+    await fetch(`${API_BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 'ok') {
+          helperText.innerText = data.message;
+          helperText.style.color = 'green';
+          email.value = '';
+          password.value = '';
+          window.location.href =
+            location.protocol + '//' + location.host + '/client/index.html';
+        }
+        if (data.status === 'fail') {
+          helperText.innerText = data.message;
+          helperText.style.color = 'red';
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- client/register/script.js
+++ client/register/script.js
@@ -1,22 +1,24 @@
-let fullName = document.getElementById("fullname");
-let username = document.getElementById("username");
-let email = document.getElementById("email");
-let phone = document.getElementById("phone");
-let password = document.getElementById("password");
-let confirmPassword = document.getElementById("confirmpassword");
+const API_BASE_URL = 'http://localhost:3001/api';
 
-let fullNameError = document.getElementById("fullnameerror");
-let usernameError = document.getElementById("usernameerror");
-let emailError = document.getElementById("emailerror");
-let phoneError = document.getElementById("phoneerror");
-let passwordError = document.getElementById("passworderror");
+let fullName = document.getElementById('fullname');
+let username = document.getElementById('username');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone');
+let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirmpassword');
 
-let successText = document.getElementById("successtext");
+let fullNameError = document.getElementById('fullnameerror');
+let usernameError = document.getElementById('usernameerror');
+let emailError = document.getElementById('emailerror');
+let phoneError = document.getElementById('phoneerror');
+let passwordError = document.getElementById('passworderror');
 
-let registerForm = document.getElementById("registerform");
-let registerButton = document.getElementById("registerbutton");
+let helperText = document.getElementById('helpertext');
 
-registerButton.addEventListener("click", (e) => {
+let registerForm = document.getElementById('registerform');
+let registerButton = document.getElementById('registerbutton');
+
+registerButton.addEventListener('click', (e) => {
   let errors = {};
 
   let fullNamePattern = /^[A-Za-z ]+$/;
@@ -25,86 +27,123 @@ registerButton.addEventListener("click", (e) => {
     /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
   let phonePattern =
     /(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?/g;
-  // let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]$/;
 
-  if (fullName.value === "") {
-    errors.fullName = "Full Name is required";
+  if (fullName.value === '') {
+    errors.fullName = 'Full Name is required';
   } else if (fullName.value.length > 20) {
-    errors.fullName = "Full Name cannot be long than 20 characters";
+    errors.fullName = 'Full Name cannot be long than 20 characters';
   } else if (fullName.value.length < 3) {
-    errors.fullName = "Full Name cannot be short than 3 characters";
+    errors.fullName = 'Full Name cannot be short than 3 characters';
   } else if (!fullNamePattern.test(fullName.value)) {
-    errors.fullName = "Invalid Full Name";
+    errors.fullName = 'Invalid Full Name';
   }
 
-  if (username.value === "") {
-    errors.username = "Username is required";
+  if (username.value === '') {
+    errors.username = 'Username is required';
   } else if (username.value.length > 20) {
-    errors.username = "Username cannot be long than 20 characters";
+    errors.username = 'Username cannot be long than 20 characters';
   } else if (username.value.length < 3) {
-    errors.username = "Username cannot be short than 3 characters";
+    errors.username = 'Username cannot be short than 3 characters';
   } else if (!usernamePattern.test(username.value)) {
-    errors.username = "Invalid Username";
+    errors.username = 'Invalid Username';
   }
 
-  if (email.value === "") {
-    errors.email = "Email is required";
+  if (email.value === '') {
+    errors.email = 'Email is required';
   } else if (!emailPattern.test(email.value)) {
-    errors.email = "Invalid Email";
+    errors.email = 'Invalid Email';
   }
 
-  if (phone.value === "") {
-    errors.phone = "Phone Number is required";
+  if (phone.value === '') {
+    errors.phone = 'Phone Number is required';
   } else if (!phonePattern.test(phone.value)) {
-    errors.phone = "Invalid Phone  Number";
+    errors.phone = 'Invalid Phone  Number';
   }
 
-  if (password.value === "") {
-    errors.password = "Password is required";
+  if (password.value === '') {
+    errors.password = 'Password is required';
   } else if (password.value.length < 8) {
-    errors.password = "Password is too short";
+    errors.password = 'Password is too short';
   } else if (password.value.length > 20) {
-    errors.password = "Password is too long";
-    // } else if (!passwordPattern.test(password.value)) {
-    //   errors.password = "Invalid Password";
+    errors.password = 'Password is too long';
   } else if (password.value !== confirmPassword.value) {
-    errors.password = "Password doesnot match";
+    errors.password = 'Password doesnot match';
   }
 
   if (Object.keys(errors).length > 0) {
     if (errors.fullName) {
       fullNameError.innerText = errors.fullName;
     } else {
-      fullNameError.innerText = "";
+      fullNameError.innerText = '';
     }
     if (errors.username) {
       usernameError.innerText = errors.username;
     } else {
-      usernameError.innerText = "";
+      usernameError.innerText = '';
     }
     if (errors.email) {
       emailError.innerText = errors.email;
     } else {
-      emailError.innerText = "";
+      emailError.innerText = '';
     }
     if (errors.phone) {
       phoneError.innerText = errors.phone;
     } else {
-      phoneError.innerText = "";
+      phoneError.innerText = '';
     }
     if (errors.password) {
       passwordError.innerText = errors.password;
     } else {
-      passwordError.innerText = "";
+      passwordError.innerText = '';
     }
   }
 
   if (Object.keys(errors).length === 0) {
-    fullNameError.innerText = "";
-    usernameError.innerText = "";
-    emailError.innerText = "";
-    phoneError.innerText = "";
-    passwordError.innerText = "";
-    successText.innerText = "Registration successful";
+    fullNameError.innerText = '';
+    usernameError.innerText = '';
+    emailError.innerText = '';
+    phoneError.innerText = '';
+    passwordError.innerText = '';
+    registerUser();
   }
 });
+
+const registerUser = async () => {
+  try {
+    await fetch(`${API_BASE_URL}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        fullname: fullName.value,
+        username: username.value,
+        email: email.value,
+        phone: phone.value,
+        password: password.value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status === 'ok') {
+          helperText.innerText = data.message;
+          helperText.style.color = 'green';
+          fullName.value = '';
+          username.value = '';
+          email.value = '';
+          phone.value = '';
+          password.value = '';
+          confirmPassword.value = '';
+        }
+        if (data.status === 'fail') {
+          helperText.innerText = data.message;
+          helperText.style.color = 'red';
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
